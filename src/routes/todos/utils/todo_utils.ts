00f6d1e77cca09a865e57e677cb31e5ae963e87a
ashank96 <bharatiashank1996@gmail.com>
2023-04-21 14:35:09 +0530

    add utility functions to get todos and count

--- src/routes/todos/utils/todo_utils.ts
+++ src/routes/todos/utils/todo_utils.ts
@@ -1,3 +1,5 @@
+import { fetchTodoList } from "./api_utils";
+
 export function isValidTodo(todo: string): boolean {
   if (todo == null || todo === undefined) {
     return false;
@@ -13,4 +15,14 @@ export function cleanTodo(todo: string): string {
   todo = todo.trim();
   todo = todo.replace(/\s+/g, " ");
   return todo;
+}
+
+export async function getTodos() {
+  var todoList = await fetchTodoList();
+  return todoList;
+}
+
+export async function getTodosCount() {
+  var todoList = await getTodos();
+  return todoList.length;
 }
